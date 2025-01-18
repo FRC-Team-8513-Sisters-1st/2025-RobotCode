22743054a8c6297a8702bf5308797d010ced2b8e
Sisters1st <89617423+Sisters1st@users.noreply.github.com>
2025-01-18 13:25:53 -0500

    Create Enums.java

---
+++ 2025-RobotCode/src/main/java/frc/robot/logic/Enums.java
@@ -0,0 +1,30 @@
+package frc.robot.logic;
+
+public final class Enums {
+    public enum ClimberStates {
+        stowed, armOut, climbing
+    }
+    public enum AlgaeGroundStates {
+        intake, holdingAlgae, outake, stowed
+    }
+    public enum AlgaeIntakeStates {
+        intake, stationary, outake
+    }
+    public enum CoralIntakeStates {
+        intake, stationary, outake
+    }
+    public enum ElevatorStates {
+        L1, L2, L3, L4,
+        stowed, scoreProcessor, L3a, L2a 
+    }
+    public enum DrivebaseStates {
+        locked, stowedLimits, L1Limits, L2Limits,
+        L3Limits, L4Limits 
+    }
+    public enum RobotStates {
+        climbDeep, preClimb, driving, algaeIntakeL3, algaeIntakeL2,
+        algaeIntakeGround, algaeScoreProcessor, coralScore1, coralScore2, coralScore3,
+        coralScore4, coralIntakeFeederSt, coral1, coral2, coral3, coral4 
+
+    }
+}
