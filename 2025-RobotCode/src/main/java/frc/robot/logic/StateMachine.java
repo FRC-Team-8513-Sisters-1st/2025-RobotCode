62985d42709525937498ece06a0661d73232d72d
Sisters1st <89617423+Sisters1st@users.noreply.github.com>
2025-01-21 16:26:41 -0500

    update stateachine (does not build)

--- 2025-RobotCode/src/main/java/frc/robot/logic/StateMachine.java
+++ 2025-RobotCode/src/main/java/frc/robot/logic/StateMachine.java
@@ -1,8 +1,8 @@
 package frc.robot.logic;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.Robot;
 import frc.robot.Settings;
-import frc.robot.logic.Enums.ElevatorStates;
 import frc.robot.logic.Enums.RobotStates;
 
 public class StateMachine {
@@ -10,7 +10,7 @@ public class StateMachine {
     Robot thisRobot;
 
     RobotStates robotState = RobotStates.driving;
-    boolean storeCoralScore;
+    Pose2d coralScoreGoalPose;
 
     public StateMachine(Robot thisRobotIn) {
 
@@ -30,18 +30,60 @@ public class StateMachine {
                 if (thisRobot.teleopController.driverXboxController.getRawButtonPressed(Settings.buttonId_Climb)) {
                     robotState = RobotStates.climbDeep;
                 }
-                if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ab)) {
+                if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ab) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_cd)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ef)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_gh)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ij)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_kl))) {
                     storeCoralScore = true;
                     if (thisRobot.teleopController.driverXboxController
                             .getRawButtonPressed(Settings.buttonId_RightBranch)) {
-                        robotState = RobotStates.coral1;
+                        robotState = RobotStates.coralScore1;
                     }
                     if (thisRobot.teleopController.driverXboxController
                             .getRawButtonPressed(Settings.buttonId_LeftBranch)) {
-                        robotState = RobotStates.coral1;
-                    }
-
+                        robotState = RobotStates.coralScore1;
+                    }               
                 }
+
+                if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ab) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_cd)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ef)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_gh)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ij)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_kl))) {
+                    storeCoralScore = true;
+                    if (thisRobot.teleopController.driverXboxController
+                            .getRawButtonPressed(Settings.buttonId_RightBranch)) {
+                        robotState = RobotStates.coralScore2;
+                    }
+                    if (thisRobot.teleopController.driverXboxController
+                            .getRawButtonPressed(Settings.buttonId_LeftBranch)) {
+                        robotState = RobotStates.coralScore2;
+                    }               
+                }
+
+                if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ab) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_cd)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ef)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_gh)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ij)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_kl))) {
+                    storeCoralScore = true;
+                    if (thisRobot.teleopController.driverXboxController
+                            .getRawButtonPressed(Settings.buttonId_RightBranch)) {
+                        robotState = RobotStates.coralScore3;
+                        if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_Algae2)) {
+                            robotState = RobotStates.algaeIntakeL2;
+                        }
+                    }
+                    if (thisRobot.teleopController.driverXboxController
+                            .getRawButtonPressed(Settings.buttonId_LeftBranch)) {
+                        robotState = RobotStates.coralScore3;
+                    }               
+                }
+
+                if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ab) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_cd)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ef)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_gh)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_ij)) || (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_kl))) {
+                    storeCoralScore = true;
+                    if (thisRobot.teleopController.driverXboxController
+                            .getRawButtonPressed(Settings.buttonId_RightBranch)) {
+                        robotState = RobotStates.coralScore4;
+                        if (thisRobot.teleopController.operatorJoystick1.getRawButtonPressed(Settings.buttonId_Algae3)) {
+                            robotState = RobotStates.algaeIntakeL3;
+                        }
+                    }
+                    if (thisRobot.teleopController.driverXboxController
+                            .getRawButtonPressed(Settings.buttonId_LeftBranch)) {
+                        robotState = RobotStates.coralScore4;
+                    }               
+                }
+
                 break;
             default:
                 break;
