f40f97f6ac94fad2d9f3f961a5c2664eb080dfc5
Sisters1st <89617423+Sisters1st@users.noreply.github.com>
2025-02-25 15:27:48 -0500

    improved elevator go up method

--- 2025-RobotCode/src/main/java/frc/robot/Settings.java
+++ 2025-RobotCode/src/main/java/frc/robot/Settings.java
@@ -195,6 +195,7 @@ public final class Settings {
 
         public static double coralScoreThold = 0.02;
         public static double otfToAPThold = 0.25;
+        public static double elevatorSafeToGoThold = 1;
 
         // elevator positions
         public static double elevatorPosStowed = 0;
